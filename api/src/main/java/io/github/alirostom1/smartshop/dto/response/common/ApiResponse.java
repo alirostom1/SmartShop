cce625b5da29b641c9229885bd1c36a93a7290f6
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-25 10:03:47 +0100

    feat : SS-5 add ApiResponse dto for global exception handler https://alirostom2201.atlassian.net/browse/SS-5

---
+++ api/src/main/java/io/github/alirostom1/smartshop/dto/response/common/ApiResponse.java
@@ -0,0 +1,20 @@
+package io.github.alirostom1.smartshop.dto.response.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private Long timestamp = System.currentTimeMillis();
+    private String path;
+    private Integer status;
+    private List<String> errors;
+}
