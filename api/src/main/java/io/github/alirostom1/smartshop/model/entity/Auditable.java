96f25d84a7b3b9ab1f075768147440e696bb4b59
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-25 15:27:37 +0100

    feat: SS-9 add auditable entity to abstract id and audit dates https://alirostom2201.atlassian.net/browse/SS-9

---
+++ api/src/main/java/io/github/alirostom1/smartshop/model/entity/Auditable.java
@@ -0,0 +1,30 @@
+package io.github.alirostom1.smartshop.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public abstract class Auditable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
