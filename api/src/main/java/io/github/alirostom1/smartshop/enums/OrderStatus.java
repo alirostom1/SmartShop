7668c9016b44d0bb5ed270e7d5bcd3095e3b0e15
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-25 09:23:02 +0100

    feat: SS-4 add enums necessary for models https://alirostom2201.atlassian.net/browse/SS-3

---
+++ api/src/main/java/io/github/alirostom1/smartshop/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package io.github.alirostom1.smartshop.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELED,
+    REJECTED
+}
---
+++ api/src/main/java/io/github/alirostom1/smartshop/enums/PaymentMethod.java
@@ -0,0 +1,7 @@
+package io.github.alirostom1.smartshop.enums;
+
+public enum PaymentMethod {
+    CASH,
+    CHECK,
+    BANK_TRANSFER
+}
