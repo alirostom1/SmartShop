73ba254154cdaf3a9629a22b8dd8b57abca3875e
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-25 15:27:04 +0100

    feat: SS-9 add simple user model https://alirostom2201.atlassian.net/browse/SS-9

---
+++ api/src/main/java/io/github/alirostom1/smartshop/model/entity/User.java
@@ -0,0 +1,25 @@
+package io.github.alirostom1.smartshop.model.entity;
+
+import io.github.alirostom1.smartshop.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Table(name = "users")
+public class User extends Auditable{
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String hashedPassword;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private UserRole role = UserRole.CLIENT;
+}
