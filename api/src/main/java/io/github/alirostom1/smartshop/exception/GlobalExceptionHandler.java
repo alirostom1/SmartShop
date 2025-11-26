1c1bad11c262ef9549923a55fb3d834bf2dfd38a
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-26 10:01:43 +0100

    feat: SS-12 add authorization exception as well as its exception handler https://alirostom2201.atlassian.net/browse/SS-12

--- api/src/main/java/io/github/alirostom1/smartshop/exception/GlobalExceptionHandler.java
+++ api/src/main/java/io/github/alirostom1/smartshop/exception/GlobalExceptionHandler.java
@@ -104,6 +104,18 @@ public class GlobalExceptionHandler{
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiResponse);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResponse<Void>> handleAccessDeniedException(
+            AccessDeniedException ex,HttpServletRequest request){
+        ApiResponse<Void> apiResponse = ApiResponse.<Void>builder()
+                .success(false)
+                .status(403)
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiResponse);
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGenericExceptions(
