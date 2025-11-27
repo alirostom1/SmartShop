8b555e7cbb72e5644d29817e81973b1a8f641dba
ali-rostom1 <ali.rostom220@gmail.com>
2025-11-27 11:12:49 +0100

    feat: add request to entity in user mapper

--- api/src/main/java/io/github/alirostom1/smartshop/model/mapper/UserMapper.java
+++ api/src/main/java/io/github/alirostom1/smartshop/model/mapper/UserMapper.java
@@ -1,13 +1,18 @@
 package io.github.alirostom1.smartshop.model.mapper;
 
 
+import io.github.alirostom1.smartshop.dto.request.client.CreateClientRequest;
 import io.github.alirostom1.smartshop.dto.response.user.UserInternalResponse;
 import io.github.alirostom1.smartshop.dto.response.user.UserPublicResponse;
 import io.github.alirostom1.smartshop.model.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserPublicResponse entityToPublicResponse(User user);
     UserInternalResponse entityToInternalResponse(User user);
+
+
+    User requestToEntity(CreateClientRequest request);
 }
